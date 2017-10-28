650f19c61741e71cc4cef93bb4bca349dbfdd9e1
01010w4 <zarif.arzimetov@gmail.com>
2017-10-28 20:27:59 +0600

    Assert added

--- src/test/java/Check.java
+++ src/test/java/Check.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.*;
 
 import java.io.File;
@@ -68,8 +69,46 @@ public class Check {
 
             wd.findElement(By.xpath("//div[@id='tabs-m']/div/div/div[3]/a/span[1]")).click();
             wd.findElement(By.linkText("Показать еще")).click();
-            System.out.println(sequenceChecker.findMask(mobNo, "0010208"));
+            Assert.assertEquals((sequenceChecker.findMask(mobNo, "0010208")), true);
         }
+
+    public boolean waitForJSandJQueryToLoad() {
+
+        WebDriverWait wait = new WebDriverWait(wd, 30);
+
+        // wait for jQuery to load
+        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+            public Object getDriver() {
+                return wd;
+            }
+
+            @Override
+            public Boolean apply(WebDriver driver) {
+                try {
+                    return ((Long)((JavascriptExecutor)getDriver()).executeScript("return jQuery.active") == 0);
+                }
+                catch (Exception e) {
+                    // no jQuery present
+                    return true;
+                }
+            }
+        };
+
+        // wait for Javascript to load
+        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+            public Object getDriver() {
+                return wd;
+            }
+
+            @Override
+            public Boolean apply(WebDriver driver) {
+                return ((JavascriptExecutor)getDriver()).executeScript("return document.readyState")
+                        .toString().equals("complete");
+            }
+        };
+
+        return wait.until(jQueryLoad) && wait.until(jsLoad);
+    }
 
         @AfterMethod
         public void tearDown() {
